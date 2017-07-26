7f0ab40ffa9e298abe240490f0fc6ec3ce43764e
Raymond Choo Juan Yong <volvagia356@gmail.com>
2017-07-27 02:26:37 +0800

    Read scraping config from JSON file

--- scrape.py
+++ scrape.py
@@ -1,24 +1,24 @@
 import requests_cache
 import json
 import logging
+from sys import argv
 
 from myprocurement import MyProcurement
 
-PROJECT_NAME = "keputusan_rundingan"
-SOURCE_AGENCY = "treasury"
-URL = "http://myprocurement.treasury.gov.my/custom/p_keputusan_rundingan.php"
-COLUMNS = ["id", "title", "category", "ministry", "agency", "company", "value", "criteria", "date"]
+config_filename = argv[1]
+with open(config_filename) as config_file:
+    config = json.load(config_file)
 
 logging.getLogger().setLevel(logging.INFO)
-requests_cache.install_cache(PROJECT_NAME)
+requests_cache.install_cache(config['project_name'])
 
-scraper = MyProcurement(URL)
-jsonl_file = open("{}.jsonl".format(PROJECT_NAME), "w")
+scraper = MyProcurement(config['url'])
+jsonl_file = open("{}.jsonl".format(config['project_name']), "w")
 
 for row, url in scraper.get_rows():
-    data = dict(zip(COLUMNS, row))
+    data = dict(zip(config['columns'], row))
     data['source_url'] = url
-    data['source_agency'] = SOURCE_AGENCY
+    data['source_agency'] = config['source_agency']
     jsonl = json.dumps(data)
     jsonl_file.write(jsonl)
     jsonl_file.write("\n")
